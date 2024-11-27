99eacc231ffb97a7796e84d9a1d404b28e4b72b0
viraj-10 <virajjoshi132@gmail.com>
2024-11-27 12:04:09 +0530

    fix: metro config

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,4 @@ const { getDefaultConfig } = require("@expo/metro-config");
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push("cjs");
 
-module.exports = withNativeWind(defaultConfig);
+module.exports = withNativeWind(defaultConfig, { input: "./global.css" });
